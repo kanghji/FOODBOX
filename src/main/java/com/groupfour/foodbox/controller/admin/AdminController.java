f31ca53316a66bb7844a65c4b029bc3d45e95ab9
sunzetz <closunzet@gmail.com>
2023-07-26 11:54:48 +0900

    First Commit
    관리자 메인 페이지

---
+++ src/main/java/com/groupfour/foodbox/controller/admin/AdminController.java
@@ -0,0 +1,15 @@
+package com.groupfour.foodbox.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    @GetMapping("")
+    public String admin() {
+        return "admin/admin_list";
+    }
+
+}
