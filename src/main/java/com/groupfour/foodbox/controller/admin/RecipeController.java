e935d15900cfed9eb7de1e8d153e98001d4fe71f
YJzzang <qmddiss@gmail.com>
2023-08-01 17:11:28 +0900

    레시피 리스트 불러오기

--- src/main/java/com/groupfour/foodbox/controller/admin/RecipeController.java
+++ src/main/java/com/groupfour/foodbox/controller/admin/RecipeController.java
@@ -1,30 +1,33 @@
-package com.groupfour.foodbox.controller.user;
+package com.groupfour.foodbox.controller.admin;
 
+import com.groupfour.foodbox.domain.RecipeDTO;
 import com.groupfour.foodbox.service.user.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 import java.util.Map;
 
 @Controller
+@RequestMapping("/admin")
 public class RecipeController {
 
     @Autowired
     private RecipeService recipeService;
 
-    @RequestMapping("/recipe")
-    public String product(Model model){
+    @GetMapping("/recipe")
+    public String recipeList(Model model){
         //Model 을 넘겨야 view에서 list를 띄울 수 있음.
         /*맵을 사용하여 data 만들기, 리스트를 service로 넘기기*/
-        List<Map<String, Object>> list = recipeService.getList();
+        List<List<RecipeDTO>> list = recipeService.getList();
 
         model.addAttribute("list",list);
 
-
-        return "/recipe/";
+        //System.out.println("list = " + list);
+        return "/admin/recipe/recipeList";
     }
 
 
--- src/main/java/com/groupfour/foodbox/service/user/RecipeServiceImpl.java
+++ src/main/java/com/groupfour/foodbox/service/user/RecipeServiceImpl.java
@@ -1,4 +1,24 @@
 package com.groupfour.foodbox.service.user;
 
-public class RecipeServiceImpl {
+import com.groupfour.foodbox.domain.RecipeDTO;
+import com.groupfour.foodbox.mapper.user.RecipeMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class RecipeServiceImpl implements RecipeService{
+    @Autowired
+    private RecipeMapper recipeMapper;
+
+    @Override
+    public List<List<RecipeDTO>> getList(){
+        //DAO영속성 계층
+        List<List<RecipeDTO>> list = recipeMapper.getList();
+
+        return list;
+    }
 }
