cc5b4db04554c9eb7d7c4d8b8febf4ff7daf6d64
kanghji <k.hjii0220@gmail.com>
2023-08-03 12:12:15 +0900

    마이페이지 컨트롤러 생성 및 위치변경

---
+++ src/main/java/com/groupfour/foodbox/controller/user/MyPageController.java
@@ -0,0 +1,37 @@
+package com.groupfour.foodbox.controller.user;
+
+import com.groupfour.foodbox.domain.UserDTO;
+import com.groupfour.foodbox.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/user")
+public class MyPageController {
+
+    // 사용자페이지 - 비밀번호 재확인 view (임시)
+    @GetMapping("/user_infoUpdateChk")
+    public String infoUpdateChk() {
+        return "user/user_infoUpdateChk";
+    }
+
+    // 사용자페이지 - 비밀번호 변경 view (임시)
+    @GetMapping("/user_pwUpdate")
+    public String pwUpdate() {
+        return "user/user_pwUpdate";
+    }
+
+    // 사용자페이지 - 회원수정정보 view
+    @GetMapping("/user_infoUpdateView")
+    public String infoUpdateView() {
+        return "user/user_infoUpdateView";
+    }
+
+}
