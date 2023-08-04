18bd5e04645b96d4bb782093d92749d7c13aa11a
YJzzang <qmddiss@gmail.com>
2023-08-04 14:41:57 +0900

    레시피 페이징에서 검색완료!
    체크박스 검색, 미완료

--- src/main/java/com/groupfour/foodbox/controller/admin/RecipeController.java
+++ src/main/java/com/groupfour/foodbox/controller/admin/RecipeController.java
@@ -18,15 +18,18 @@ public class RecipeController {
     private RecipeService recipeService;
 
     //레시피 리스트 페이지 연결
-    @GetMapping("/recipe")
-    public String recipeList(@ModelAttribute("pageDTO") PageDTO pageDTO, Model model){
+    @RequestMapping("/recipe")
+    public String recipeList(@RequestParam(value = "RCP_NM", required = false)String RCP_NM,
+            @ModelAttribute("pageDTO") PageDTO pageDTO, Model model){
         //Model 을 넘겨야 view에서 list를 띄울 수 있음.
         /*맵을 사용하여 data 만들기, 리스트를 service로 넘기기*/
-        List<List<RecipeDTO>> list = recipeService.getList(pageDTO);
-
+        List<List<RecipeDTO>> list = recipeService.getList(RCP_NM,pageDTO);
+        System.out.println("RCP_NM = " + RCP_NM);
         model.addAttribute("list",list);
         model.addAttribute("pageDTO", pageDTO);
         //System.out.println("list = " + list);
+        pageDTO.setRCP_NM(RCP_NM);
+        System.out.println("pageDTO.getRCP_NM() = " + pageDTO.getRCP_NM());
         return "/admin/recipe/recipeList";
     }
 
@@ -40,16 +43,18 @@ public class RecipeController {
         return "/admin/recipe/recipeInfo";
     }
     //메뉴 이름 검색
-    @PostMapping("/recipe/recipeSearch")
-    public String recipeSearch(@RequestParam(value = "RCP_NM", defaultValue = "ALL")String RCP_NM,
-                               @ModelAttribute("pageDTO") PageDTO pageDTO,
-                                                Model model){
-        List<List<RecipeDTO>> list = recipeService.recipeSearch(RCP_NM, pageDTO);
-        model.addAttribute("list", list);
-
-        return "/admin/recipe/recipeList";
-
-    }
+//    @RequestMapping("/recipe/recipeSearch")
+//    public String recipeSearch(@RequestParam(value = "RCP_NM", defaultValue = "ALL")String RCP_NM,
+//                               @ModelAttribute("pageDTO") PageDTO pageDTO,
+//                                                Model model){
+//        List<List<RecipeDTO>> list = recipeService.recipeSearch(RCP_NM, pageDTO);
+//        model.addAttribute("list", list);
+//        model.addAttribute("pageDTO", pageDTO);
+//        System.out.println("pageDTO.getRCP_NM() = " + pageDTO.getRCP_NM());
+//
+//        return "/admin/recipe/recipeList";
+//
+//    }
 
 
 
