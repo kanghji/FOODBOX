39adc6e5a6941cacdfaa6dd8262b3507c8aae205
sunzetz <closunzet@gmail.com>
2023-08-07 12:47:14 +0900

    User 주문하기 추가_2

--- src/main/resources/static/js/userOrder.js
+++ src/main/resources/static/js/userOrder.js
@@ -1,37 +1,30 @@
-let userOrderObject = {
 
-    insertCart: function (prod_code, user_id) {
+function orderBtn () {
+    requestPay();
 
-        let order_qty = $("#order_qty").val();
-
-        $.ajax({
-            type: "post",
-            url: "/user/addCart",
-            data: JSON.stringify({user_id: user_id, prod_code: prod_code, order_qty: order_qty}),
-            contentType: "application/json; charset=utf-8"
-        }).done(function (response) {
-            location.href = "/user/userCartList/" + user_id;
-        }).fail(function () {
-            alert("로그인이 필요합니다!");
-        });
-    },
-
-    deleteCart: function (cart_no) {
-        if(confirm("장바구니에서 삭제하시겠습니까?") == true) {
-            $.ajax({
-                type: "post",
-                url: "/user/userCartDelete",
-                data: JSON.stringify({cart_no: cart_no}),
-                contentType: "application/json; charset=utf-8"
-            }).done(function (response) {
-                alert("삭제되었습니다");
-                document.location.href = document.location.href;
-            }).fail(function (error) {
-                alert("에러발생 : " + error);
-            });
-        } else {
-            return;
-        }
+    function requestPay() {
+        IMP.init("");
+        IMP.request_pay({
+                pg: "html5_inicis",		//KG이니시스 pg파라미터 값
+                pay_method: "card",		//결제 방법
+                merchant_uid: "1234578",//주문번호
+                name: "당근 10kg",		//상품 명
+                amount: 200,			//금액
+                buyer_email: "gildong@gmail.com",
+                buyer_name: "홍길동",
+                buyer_tel: "010-4242-4242",
+                buyer_addr: "서울특별시 강남구 신사동",
+                buyer_postcode: "01181"
+            },
+            function (rsp) {
+                //rsp.imp_uid 값으로 결제 단건조회 API를 호출하여 결제결과를 판단합니다.
+                if (rsp.success) {
+                    alert("결제성공");
+                    //서버 검증 요청 부분
+                } else {
+                    alert("결제에 실패하였습니다. 에러 : " + rsp.error_msg);
+                }
+            }
+        );
     }
-
 }
