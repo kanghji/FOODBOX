8f5142a76163e577e2b103793f55910344e04d7e
sunzetz <closunzet@gmail.com>
2023-08-08 11:29:45 +0900

    User 주문하기 추가_3

--- src/main/resources/static/js/userOrder.js
+++ src/main/resources/static/js/userOrder.js
@@ -1,30 +1,111 @@
+function orderBtn() {
 
-function orderBtn () {
+    let pg;
+    let payMethod;
+    let buyerEmail = document.getElementById('user_email').textContent;
+    let buyerName = document.getElementById('user_name').textContent;
+    let buyerTel = document.getElementById('user_tel').textContent;
+    let buyerAddr = document.getElementById('user_addr').textContent;
+    let buyerPostcode = document.getElementById('user_zipcode').textContent;
+    let amount = document.getElementById('order_qty').value;
+    let merchantUid;
+    let name = document.getElementById('prod_name').textContent();
+
     requestPay();
+
+    function paymentRecord() {
+        // pg = document.getElementById('pg')[document.getElementById('pg')
+        //     .selectedIndex].value.toUpperCase();
+        // if (pg == '결제 방법 선택') {
+        //     alert('결제 방법을 선택해주세요');
+        //     preventDefault();
+        // }
+        // payMethod = document.getElementById('payMethod')[document.getElementById('payMethod')
+        //     .selectedIndex].value.toUpperCase();
+        // if (payMethod == '결제 수단 선택') {
+        //     alert('결제 수단을 선택해주세요');
+        //     preventDefault();
+        // }
+        buyerEmail = document.getElementById('user_email').textContent;
+        buyerName = document.getElementById('user_name').textContent;
+        buyerTel = document.getElementById('user_tel').textContent;
+        buyerAddr = document.getElementById('user_addr').textContent;
+        buyerPostcode = document.getElementById('user_zipcode').textContent;
+
+        let data = {
+            method: 'POST',
+            body: JSON.stringify({
+                // pg,
+                // payMethod,
+                buyerEmail,
+                buyerName,
+                buyerTel,
+                buyerAddr,
+                buyerPostcode
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        };
+        const onSuccess = res => {
+            res.json()
+                .then((json) => {
+                    amount = json.amount;
+                    merchantUid = json.merchantUid;
+                })
+        }
+        const onFailure = res => {
+            return res.json()
+                .then(json => {
+                    alert(json.message);
+                    preventDefault();
+                })
+        }
+
+        fetch(`/api/v1/payment/record`, data)
+            .then(res => {
+                if (!res.ok) {
+                    throw res;
+                }
+                return res
+            })
+            .then(onSuccess, onFailure)
+            .catch(err => {
+                console.log(err.message);
+            });
+    };
 
     function requestPay() {
         IMP.init("");
         IMP.request_pay({
-                pg: "html5_inicis",		//KG이니시스 pg파라미터 값
-                pay_method: "card",		//결제 방법
-                merchant_uid: "1234578",//주문번호
-                name: "당근 10kg",		//상품 명
-                amount: 200,			//금액
-                buyer_email: "gildong@gmail.com",
-                buyer_name: "홍길동",
-                buyer_tel: "010-4242-4242",
-                buyer_addr: "서울특별시 강남구 신사동",
-                buyer_postcode: "01181"
+                pg: "html5_inicis",
+                pay_method: "card",
+                merchant_uid: merchantUid,
+                amount: amount,
+                name: name,
+                buyer_email: buyerEmail,
+                buyer_name: buyerName,
+                buyer_tel: buyerTel,
+                buyer_addr: buyerAddr,
+                buyer_postcode: buyerPostcode,
+                m_redirect_url: "/order/payment-result"
             },
             function (rsp) {
-                //rsp.imp_uid 값으로 결제 단건조회 API를 호출하여 결제결과를 판단합니다.
                 if (rsp.success) {
-                    alert("결제성공");
-                    //서버 검증 요청 부분
+                    jQuery.ajax({
+                        url: "/api/v1/payment/complete", // 예: https://www.myservice.com/payments/complete
+                        method: "POST",
+                        headers: {"Content-Type": "application/json"},
+                        data: {
+                            imp_uid: rsp.imp_uid,
+                            merchant_uid: rsp.merchant_uid
+                        }
+                    }).done(function (data) {
+                        location.replace('/order/payment-result');
+                    });
                 } else {
                     alert("결제에 실패하였습니다. 에러 : " + rsp.error_msg);
                 }
-            }
-        );
+            });
     }
 }
