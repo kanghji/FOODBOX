a52ef74c272fac73ae35d842afa77d981ac7f770
kanghji <k.hjii0220@gmail.com>
2023-08-15 13:41:22 +0900

    footer 완료

--- src/main/java/com/groupfour/foodbox/controller/user/SearchController.java
+++ src/main/java/com/groupfour/foodbox/controller/user/SearchController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Map;
